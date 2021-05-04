0282efbc9b3e0f848f3e768d8341657ed78c6a84
mansii-shah <mansishah21179@gmail.com>
2021-05-04 16:07:56 +0530

    initial commit

---
+++ src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './home-page.component.scss';
+const HomePage=()=>{
+        return (
+            <div className="homepage">
+                <div className="directory-menu">
+                    <div className="menu-item">
+                        <div className="content">
+                            <h2 className="title">Hats</h2>
+                            <h2 className="subtitle">Shop Now</h2>
+                        </div>
+                    </div>
+                    <div className="menu-item">
+                        <div className="content">
+                            <h2 className="title">Hats</h2>
+                            <h2 className="subtitle">Shop Now</h2>
+                        </div>
+                    </div>
+                    <div className="menu-item">
+                        <div className="content">
+                            <h2 className="title">Hats</h2>
+                            <h2 className="subtitle">Shop Now</h2>
+                        </div>
+                    </div>
+                    <div className="menu-item">
+                        <div className="content">
+                            <h2 className="title">Hats</h2>
+                            <h2 className="subtitle">Shop Now</h2>
+                        </div>
+                    </div>
+                    <div className="menu-item">
+                        <div className="content">
+                            <h2 className="title">Hats</h2>
+                            <h2 className="subtitle">Shop Now</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+}
+export default HomePage;
