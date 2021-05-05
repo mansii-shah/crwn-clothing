25dc45de6314853f49b4bb680e5a5273ee3518b5
mansii-shah <mansishah21179@gmail.com>
2021-05-05 17:56:14 +0530

    add collection item component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import CollectionItems from '../collection-items/collection-items.component';
+import './collection-preview.component.scss';
 const CollectionPreview=({title,items})=>{
     return (
             <div className="collection-preview">
                <h5 className="title">
                    {title}
                </h5>
-               <div className="collection-items">
+               <div className="preview">
                 {
-                    items.map(item=>(
-                        <div key={item.id}>
-                            {item.name}
-                        </div>
+                    items.slice(0,4).map(({id,...otherItems})=>(
+                        <CollectionItems key={id} {...otherItems} ></CollectionItems>
+                      
                     ))
                 }
                </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,7 +6,8 @@ import './menu-item.component.scss';
 const MenuItem=({title,url,size,history,linkUrl,match})=>{
         return (
                 <div className={`${size} menu-item` }  onClick={()=>history.push(`${match.url}${linkUrl}`)}>
-                    <div className="background-image"  style={{backgroundImage: 'url(' + url + ')'}} />
+                    <div className="background-image"  
+                    style={{backgroundImage: 'url(' + url + ')'}} />
             <div className="content">
                     <h2 className="title">{title}</h2>
                     <h2 className="subtitle">Shop Now</h2>
