7e91490eb6169ce08f189c171d7477b3b3665ef9
mansii-shah <mansishah21179@gmail.com>
2021-05-07 18:31:31 +0530

    change in sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,11 +1,33 @@
  import React ,{useState} from 'react';
 const SignInComponent=()=>{
-    const [email]=useState();
-    const [password]=useState();
+    const [email,setEmail]=useState();
+    const [password,setPassword]=useState();
 
+   function handleSubmit(event){
+        event.preventDefault();
+        
+    }
+    function handleChange(event){
+        if(event.target.name==='email'){
+                setEmail(event.target.value);
+        }else{
+            setPassword(event.target.value);
+        }
+
+    }
     return(
+        
         <div className="sign-in">
             <h2>I have Already Acount</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+            <label>Enter Email</label>
+                <input type="email" name="email" onChange={handleChange} value={email} placeholder="email"></input>
+                <label>Enter Password</label>
+                <input type="password" name="password" onChange={handleChange} value={password} placeholder="Password"></input>
+               
+                <input type="submit" value="submit" />
+            </form>
         </div>
     )
 
--- src/pages/authentication-page/authentication-page.component.jsx
+++ src/pages/authentication-page/authentication-page.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './authentication-page.component.scss';
+import SignInComponent from '../../components/sign-in/sign-in.component';
 
 const AuthenticationPage=()=>{
     return(
         <div className="sign-in and sign-up">
-            
+           <SignInComponent></SignInComponent>
         </div>
     )
 }
